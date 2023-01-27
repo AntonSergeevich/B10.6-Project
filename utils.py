849e509ac2611e6cb99c3438f62f37c35ac7f16b
Anton <glukhovas@yandex.ru>
2023-01-27 23:36:28 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,35 +1,34 @@
 import json
 import requests
-from config import*
-from app import*
+from config import keys
 
-class ConvertionExсeption(Exception):
+
+class ConvertionException(Exception):
     pass
+
 
 class CryptoConverter():
     @staticmethod
     def convert(quote: str, base: str, amount: str):
         if quote == base:
-            raise ConvertionExсeption(
+            raise ConvertionException(
                 f'Вы переводите валюту {base} в валюту в такую же. Совсем что ли?\nВалюты должны быть разные!')
 
         try:
             quote_ticker = keys[quote]
         except KeyError:
-            raise ConvertionExсeption(f'Не удалось обработать валюту {quote}')
+            raise ConvertionException(f'Не удалось обработать валюту {quote}')
 
         try:
             base_ticker = keys[base]
         except KeyError:
-            raise ConvertionExсeption(f'Не удалось обработать валюту {base}')
+            raise ConvertionException(f'Не удалось обработать валюту {base}')
 
         try:
             amount = float(amount)
         except ValueError:
-            raise ConvertionExсeption(f'Не удалось обработать количество {amount}')
-
+            raise ConvertionException(f'Не удалось обработать количество {amount}')
 
         r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
-
-
-
+        total_base = json.loads(r.content)[keys[base]]
+        return total_base
